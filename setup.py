a0dfe4707374461efeebe40a726ba6fdb7a2c06d
Yannick Versley <yversley@googlemail.com>
2015-09-21 14:45:06 +0200

    added information to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+README_TEXT = file('README.md').read()
+
 setup(name='pathconfig',
       version='1.0',
       description='On-demand loading and configurable paths',
+      long_description =README_TEXT,
       author='Yannick Versley',
       author_email='versley@cl.uni-heidelberg.de',
       packages=['pathconfig'],
       package_dir={'': 'py_src'},
+      url='https://github.com/yv/pathconfig',
       install_requires=['setuptools>=17', 'pyyaml']
       )
